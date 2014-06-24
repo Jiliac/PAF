7e0a700e97370f9b85181b71b3eb0a606602f939
Jiliac <v.manes94@gmail.com>
2014-06-24 09:31:34 +0200

    analysis is ready

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-folderpath <- "~/dev/PAF/data/"
+folderpath <- "~/Development/PAF/data/"
 
 dfir <- "2dfir-O3-80.0M-25-EnergyData-"
 blowfish <- "2dfir-O3-80.0M-25-EnergyData-"
@@ -13,25 +13,25 @@ sha <- "sha-O3-32.0M-25-EnergyData-"
 filenametab = c(dfir, blowfish, crc, cubic, dijkstra, fdct, float, int, rijndael, sha)
 
 plotdata <- function(filename){
-	frequencies <- c("1.0" ,"1.1" ,"1.2" ,"1.3" ,"1.4" ,"1.5" ,"1.6" )
-	power <- matrix(ncol=6)
-	energy <- matrix(ncol=6)
-	time <- matrix(ncol=6)
+	frequencies <- c("0.25", "0.3", "0.35", "0.4", "0.5", "0.6", "0.8", "0.9", "1.0" ,"1.1" ,"1.2" ,"1.3" ,"1.4" ,"1.5" ,"1.6" )
+	power <- matrix(ncol=15)
+	energy <- matrix(ncol=15)
+	time <- matrix(ncol=15)
 	i <- 1
 	for (frequency in frequencies) {
 		filepath <- paste(c(folderpath, filename, frequency, "Ghz.txt"), collapse='')
 		mydata <- read.table(filepath)
-		power[i] <- mean(mydata[,23])
+		power[i] <- mean(mydata[,20] + mydata[,23])
 
 		time[i] <- mydata[nrow(mydata), 1] - mydata[1,1] 
 		energy[i] <- time[i] * power[i]
 		i <- i + 1
 	}
-	plot(x=frequencies, y=energy)
+	plot(x=frequencies, y=energy, main=filename)
 }
 
-plotdata(rijndael)
-#for(filename in filenametab) {
-#	plotdata(filename)
-#	Sys.sleep(2)
-#}
+#plotdata(rijndael)
+for(filename in filenametab) {
+	plotdata(filename)
+	Sys.sleep(2)
+}
