1941d0df81dfac6221a71e7b1a31c947f6089850
Manes <v.manes94@gmail.com>
2014-06-24 08:00:54 +0200

    better plots

--- analysis.R
+++ analysis.R
@@ -1,6 +1,6 @@
 folderpath <- "~/dev/PAF/data/"
 
-#2dfir <- "2dfir-O3-80.0M-25-EnergyData-"
+dfir <- "2dfir-O3-80.0M-25-EnergyData-"
 blowfish <- "2dfir-O3-80.0M-25-EnergyData-"
 crc <-"crc32-O3-300.0M-25-EnergyData-"
 cubic <- "cubic-O3-50.0M-25-EnergyData-"
@@ -10,21 +10,28 @@ float <- "float_matmult-O3-18.0M-25-EnergyData-"
 int <- "int_matmult-O3-6.5M-25-EnergyData-"
 rijndael <- "rijndael-O3-150.0M-25-EnergyData-"
 sha <- "sha-O3-32.0M-25-EnergyData-"
+filenametab = c(dfir, blowfish, crc, cubic, dijkstra, fdct, float, int, rijndael, sha)
 
-filename <- sha
-frequencies <- c("1.0" ,"1.1" ,"1.2" ,"1.3" ,"1.4" ,"1.5" ,"1.6" )
-power <- matrix(ncol=6)
-energy <- matrix(ncol=6)
-time <- matrix(ncol=6)
-i <- 1
-for (frequency in frequencies) {
-	filepath <- paste(c(folderpath, filename, frequency, "Ghz.txt"), collapse='')
-	mydata <- read.table(filepath)
-	power[i] <- mean(mydata[,23])
-	
-	time[i] <- mydata[nrow(mydata), 1] - mydata[1,1] 
-	energy[i] <- time[i] * power[i]
-	i <- i + 1
+plotdata <- function(filename){
+	frequencies <- c("1.0" ,"1.1" ,"1.2" ,"1.3" ,"1.4" ,"1.5" ,"1.6" )
+	power <- matrix(ncol=6)
+	energy <- matrix(ncol=6)
+	time <- matrix(ncol=6)
+	i <- 1
+	for (frequency in frequencies) {
+		filepath <- paste(c(folderpath, filename, frequency, "Ghz.txt"), collapse='')
+		mydata <- read.table(filepath)
+		power[i] <- mean(mydata[,23])
+
+		time[i] <- mydata[nrow(mydata), 1] - mydata[1,1] 
+		energy[i] <- time[i] * power[i]
+		i <- i + 1
+	}
+	plot(x=frequencies, y=energy)
 }
 
-plot(x=frequencies, y=energy)
+plotdata(rijndael)
+#for(filename in filenametab) {
+#	plotdata(filename)
+#	Sys.sleep(2)
+#}
